ed803e1c4a08410a775bea020044ab71123665ac
Khyati12334 <74969670+Khyati12334@users.noreply.github.com>
2021-02-18 22:43:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+var allContestants;
+var answer;
+var database;
+var canvas, backgroundImage;
+var contestantCount;
+var gameState = 0;
+
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(1200,550);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("YELLOW");
+  if(contestantCount === 4){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
